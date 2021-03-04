19792107d1897863276f811d6f0eeed32439c1e0
JoeBig7 <1479762535@qq.com>
2021-03-04 14:06:51 +0800

    :sparkles: index

--- 2021-03-01(深入浅出索引).sql
+++ 2021-03-01(深入浅出索引).sql
@@ -14,3 +14,35 @@ alter table demo3 add PRIMARY KEY(id);
 
 ## 重建主键id
 alter table demo3 ENGINE=INNODB;
+
+
+## demo2
+create table demo4 (
+ID int primary key,
+k int NOT NULL DEFAULT 0, 
+s varchar(16) NOT NULL DEFAULT '',
+index k(k))
+engine=InnoDB;
+ 
+insert into demo4 values(100,1, 'aa'),(200,2,'bb'),(300,3,'cc'),(500,5,'ee'),(600,6,'ff'),(700,7,'gg');
+
+select * from demo4 where k between 3 and 5
+
+EXPLAIN SELECT *from demo4 where id=100;
+
+
+## demo5
+
+CREATE TABLE `demo5` (
+  `a` int(11) NOT NULL,
+  `b` int(11) NOT NULL,
+  `c` int(11) NOT NULL,
+  `d` int(11) NOT NULL,
+  PRIMARY KEY (`a`,`b`),
+  KEY `c` (`c`),
+  KEY `ca` (`c`,`a`),
+  KEY `cb` (`c`,`b`)
+) ENGINE=InnoDB;
+
+select * from demo5 where c=N order by a limit 1;
+select * from demo5 where c=N order by b limit 1;
